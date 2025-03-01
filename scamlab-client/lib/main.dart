864f6b0cec4ba099bd5049629ad743b3ad27784c
Richard Tafurth-Garcia <projects@cdstm.ch>
2025-03-01 20:12:21 +0000

    Created both a basic home page, but also get rid of the rules page.

--- scamlab-client/lib/main.dart
+++ scamlab-client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scamlab/view/page/HomePage.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const HomePage(),
+        //'/signup': (BuildContext context) => const SignUpPage(),
+      }
     );
   }
 }
