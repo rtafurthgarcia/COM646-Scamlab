8f4e01dcff6e6ed1ac02f467669f60513e9cc00e
Richard Tafurth-Garcia <projects@cdstm.ch>
2025-03-03 22:54:26 +0000

    Cleaned some unnecessary stuff

--- scamlab-server/src/main/java/resource/ConversationWSResource.java
+++ scamlab-server/src/main/java/resource/ConversationWSResource.java
@@ -6,20 +6,15 @@ import io.quarkus.security.Authenticated;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.websockets.next.OnClose;
 import io.quarkus.websockets.next.OnOpen;
-import io.quarkus.websockets.next.PathParam;
 import io.quarkus.websockets.next.WebSocket;
 import io.quarkus.websockets.next.WebSocketConnection;
 import io.quarkus.websockets.next.runtime.ConnectionManager;
 import jakarta.inject.Inject;
-import service.PlayerService;
 
 @Authenticated
 @WebSocket(path = "/ws/conversation/start")
 public class ConversationWSResource {
     public record ChatMessage(long numberOfPlayersConnected) {}
-
-    @Inject
-    PlayerService service;
 
     @Inject
     Logger logger;
@@ -37,12 +32,12 @@ public class ConversationWSResource {
     public ChatMessage onOpen() {
         logger.info(securityIdentity.getPrincipal().getName() + " successfully authenticated");
         logger.info("New WS connection: " + connection.endpointId());
-        return new ChatMessage(connectionManager.getConnections("*").size());
+        return new ChatMessage(connectionManager.listAll().size());
     }
 
     @OnClose
     public void onClose() {
         logger.info("WS connection closed: " + connection.endpointId());
-        connection.broadcast().sendTextAndAwait(new ChatMessage(connectionManager.getConnections("*").size()));
+        connection.broadcast().sendTextAndAwait(new ChatMessage(connectionManager.listAll().size()));
     }
 }
