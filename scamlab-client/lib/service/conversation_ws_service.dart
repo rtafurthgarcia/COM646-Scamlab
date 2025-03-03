89dfef550cda9de36819b7b3d19b3168232146d8
adidal <hello@cdstm.ch>
2025-03-03 22:47:18 +0000

    Websocket authentication finally works => uses subprotocols to share jwt tokens

--- scamlab-client/lib/service/conversation_ws_service.dart
+++ scamlab-client/lib/service/conversation_ws_service.dart
@@ -6,21 +6,32 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 class ConversationWSService {
   final String wsUrl;
   String? jwtToken;
-  late final WebSocketChannel? _channel;
+  WebSocketChannel? _channel;
 
   ConversationWSService({required this.wsUrl, this.jwtToken});
 
   bool isListening() {
-    return _channel != null && _channel.protocol != null;
+    return _channel != null && _channel!.protocol != null;
   }
 
-  /// Connects to the WebSocket and listens for messages.
   void connect(void Function(ConversationStartMessage) onMessage) {
     if (jwtToken == null) {
       throw Exception("Missing JWT token for WebSocket!");
     }
 
-    _channel = WebSocketChannel.connect(Uri.parse("$wsUrl?authorization=$jwtToken"));
+    // Encode the token in the Quarkus-specific subprotocol format,
+    // per https://quarkus.io/guides/websockets-next-reference#bearer-token-authentication
+    final quarkusHeaderProtocol = Uri.encodeComponent(
+      "quarkus-http-upgrade#Authorization#Bearer $jwtToken"
+    );
+
+    // Define the subprotocols list (order matters!)
+    final protocols = [
+      "bearer-token-carrier", // Your custom subprotocol
+      quarkusHeaderProtocol,  // Quarkus header subprotocol
+    ];
+
+    _channel = WebSocketChannel.connect(Uri.parse(wsUrl), protocols: protocols);
 
     _channel?.stream.listen((data) {
       // Assume the incoming data is in JSON format.
