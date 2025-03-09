bd76380a79694f0248eb365dabfc79c4223529a1
Richard Tafurth-Garcia <projects@cdstm.ch>
2025-03-09 12:53:56 +0000

    Wrote a mapper that might be completely useless. Will probably get rid of it but still

--- scamlab-server/src/main/java/model/dto/GameMapper.java
+++ scamlab-server/src/main/java/model/dto/GameMapper.java
@@ -1,13 +1,49 @@
 package model.dto;
 
+import java.lang.reflect.Type;
+import java.util.Set;
+
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.quarkus.logging.Log;
+import io.quarkus.websockets.next.BinaryMessageCodec;
+import io.vertx.core.buffer.Buffer;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Singleton;
 import jakarta.json.JsonObject;
+import model.dto.GameDto.GameCallToVoteMessageDto;
+import model.dto.GameDto.GameCastVoteMessageDto;
+import model.dto.GameDto.GameFinishedMessageDto;
+import model.dto.GameDto.GameGameCancelledMessageDto;
+import model.dto.GameDto.StartMenuStatisticsMessageDto;
 import model.dto.GameDto.WSMessageType;
+import model.dto.GameDto.WaitingLobbyGameStartingMessageDto;
+import model.dto.GameDto.WaitingLobbyStatisticsMessageDto;
+import model.dto.GameDto.WaitingLobbyVoteAcknowledgedMessageDto;
+import model.dto.GameDto.WaitingLobbyVoteToStartMessageDto;
 
-@ApplicationScoped
-public class GameMapper {
+@Singleton
+public class GameMapper implements BinaryMessageCodec<Record> {
+    private static final Set<Type> SUPPORTED_TYPES = Set.of(
+        StartMenuStatisticsMessageDto.class,
+        WaitingLobbyStatisticsMessageDto.class,
+        WaitingLobbyVoteToStartMessageDto.class,
+        WaitingLobbyVoteAcknowledgedMessageDto.class,
+        WaitingLobbyGameStartingMessageDto.class,
+        GameGameCancelledMessageDto.class,
+        GameCallToVoteMessageDto.class,
+        GameCastVoteMessageDto.class,
+        GameFinishedMessageDto.class
+    );
+
+    ObjectMapper mapper;
+
+    public GameMapper() {
+        mapper = new ObjectMapper();
+    }
+
     public static Record getWSMessage(JsonObject jsonObject) throws JsonParseException {
         if (jsonObject.containsKey("type")) {
             switch (WSMessageType.valueOf(jsonObject.getString("type"))) {
@@ -28,5 +64,27 @@ public class GameMapper {
         } else {
             throw new JsonParseException("JSON doesn't correspond to any recognised WS message");
         }
+    }
+
+    @Override
+    public boolean supports(Type type) {
+        return SUPPORTED_TYPES.contains(type);
+    }
+    
+
+    @Override
+    public Buffer encode(Record value) {
+        try {
+            return Buffer.buffer(mapper.writeValueAsString(value));
+        } catch (JsonProcessingException e) {
+            Log.error(e);
+            return null;
+        }
+    }
+
+    @Override
+    public Record decode(Type type, Buffer value) {
+        throw new UnsupportedOperationException();
+        //json.to
     }
 }
