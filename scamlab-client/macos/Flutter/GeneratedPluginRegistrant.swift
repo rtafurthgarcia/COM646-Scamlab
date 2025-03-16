139084bcd38e2dfbfdbd682ef13b91e6d4df8029
Richard Tafurth-Garcia <111584774+rtafurthgarcia@users.noreply.github.com>
2025-03-16 21:57:33 +0000

    Added a new plugin that prevents the game from being closed without confirmation.

--- scamlab-client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ scamlab-client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_window_close
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWindowClosePlugin.register(with: registry.registrar(forPlugin: "FlutterWindowClosePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
